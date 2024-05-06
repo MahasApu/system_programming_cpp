ab8c78b17c106fe3c0297dd6a2f02db7b43d83d9
MahasApu <maryvask0@yandex.ru>
2024-05-06 22:37:26 +0700

    upd the 11th nstt

--- tasks/task11/GetPrimes.hpp
+++ tasks/task11/GetPrimes.hpp
@@ -1,25 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <type_traits>
 
-
 using false_t = std::integral_constant<bool, false>; 
-using true_t = std::integral_constant<bool, true>; 
+using true_t = std::integral_constant<bool, true>;
 
-template <size_t n1, size_t n2>
-struct is_prime_loop: std::conditional_t<(n1 % n2 != 0), is_prime_loop<n1, n2 - 1>, false_t> { };
+template <size_t x, size_t y>
+struct has_divisor: std::conditional_t<(x % y == 0), true_t, has_divisor<x, y + 1>> { };
 
-template <size_t n1>
-struct is_prime_loop<n1, 1>: true_t { };
-
-template <size_t n>
-struct is_prime: is_prime_loop<n, n - 1> { };
+template <size_t x>
+struct has_divisor<x, x>: false_t { };
 
 template <size_t n>
-struct get_prime: std::conditional_t<is_prime<n>::value, std::integral_constant<size_t, n>, get_prime<n + 1>> { };
+struct is_prime: std::conditional_t<has_divisor<n, 2>{}, false_t, true_t> { };
 
 template <size_t n>
-struct prime: get_prime<prime<n - 1>::value + 1> { };
+struct get_prime: std::conditional_t<is_prime<n>{}, std::integral_constant<size_t, n>, get_prime<n + 1>> { };
+
+template <size_t n>
+struct prime: get_prime<prime<n - 1>{} + 1> { };
 
 template <>
 struct prime<1>: std::integral_constant<size_t, 2> { };
