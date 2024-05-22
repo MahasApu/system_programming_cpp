29df3797fb8c8e913dc46088531862e81ad53448
MahasApu <maryvask0@yandex.ru>
2024-05-22 15:42:08 +0700

    fix the 14th nstt

--- tasks/task14/TinyAllocator.hpp
+++ tasks/task14/TinyAllocator.hpp
@@ -1,29 +1,29 @@
 #pragma once
-
+#include <iostream>
 #include <type_traits>
 
 template <typename... Args>
-constexpr bool is_copy_constructible(){
-    return (std::is_constructible<Args, const Args&>::value && ...);
-} 
+constexpr bool is_copy_constructible = (std::is_constructible<Args>::value && ...);
 
 template <size_t SIZE, size_t ALLOCATED>
-constexpr bool is_enough_memory() { return SIZE >= ALLOCATED; }
+constexpr bool is_enough_memory = SIZE >= ALLOCATED;
 
 template <typename... Args>
-constexpr size_t get_size() {
-    if (sizeof...(Args) == 0) return 0;
-    return (sizeof(Args) + ...);
+constexpr size_t get_size = sizeof...(Args) == 0 ? 0 : (sizeof(Args) + ...);
+
+template <typename T>
+constexpr char* tiny_allocator(char* ptr, T arg) {
+    ::new(ptr) T(arg);
+    ptr += sizeof(T);
+    return ptr;
 }
 
 template <size_t SIZE, typename... Types>
 void allocate(void* memory, Types... args) {
-    static_assert(is_copy_constructible<Types...>());
-    constexpr size_t allocated_mem = get_size<Types...>();
-    static_assert(is_enough_memory<SIZE, allocated_mem>);
+    
+    static_assert(is_copy_constructible<Types...>, "NOT COPY CONSTRUCTIBLE");
+    static_assert(is_enough_memory<SIZE, get_size<Types...>>, "NO FREE MEMORY");
 
-    // TODO: use placement new
     auto* ptr = static_cast<char*>(memory);
-    std::ptrdiff_t i = 0;
-    (::new(ptr++) Types(), ...);
+    ((ptr = tiny_allocator(ptr, args)), ...);
 }
--- tests/task14/task14.cpp
+++ tests/task14/task14.cpp
@@ -1,10 +1,30 @@
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <iostream>
 #include "../../tasks/task14/TinyAllocator.hpp"
 
 
+TEST(TinyAllocator, test1) {
+
+    int a = 10;
+    double b = 11;
+    char c = 's';
+
+    const size_t size = sizeof(a) + sizeof(b) + sizeof(c);
+    char mem[size];
+
+    allocate<sizeof(mem)>(mem, a, b, c);
+
+    int* a_ = (int*) (mem);
+    double* b_ = (double*) (mem + sizeof(int));
+    char* c_ = (char*) (mem + sizeof(int) + sizeof(double));
+
+    ASSERT_TRUE(a == *a_);
+    ASSERT_TRUE(b == *b_);
+    ASSERT_TRUE(c == *c_);
+}
+
 int main() {
-    int a;
-    double b;
-    char c;
-    allocate<100>(&a, a, b, c);
-    return 0;
+
 }
